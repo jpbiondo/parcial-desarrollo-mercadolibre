0cb34117be26f91f06f24c215aa903e037bc793d
Juan Pablo <jupbiondo@gmail.com>
2024-09-29 12:07:17 -0300

    Add dependecy MutanteService for MutanteController and Mutante variable for request body

--- src/main/java/com/jpbiondo/mutanteAPI/controllers/MutanteController.java
+++ src/main/java/com/jpbiondo/mutanteAPI/controllers/MutanteController.java
@@ -1,5 +1,8 @@
 package com.jpbiondo.mutanteAPI.controllers;
 
+import com.jpbiondo.mutanteAPI.entities.Mutante;
+import com.jpbiondo.mutanteAPI.services.MutanteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,11 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "")
 @CrossOrigin(origins = "*")
 public class MutanteController {
+    private MutanteService mutanteService;
+    @Autowired
+    public MutanteController(MutanteService mutanteService){
+        this.mutanteService = mutanteService;
+    }
 
     @PostMapping("/mutant")
-    public ResponseEntity<?> isMutant(@RequestBody) {
+    public ResponseEntity<?> isMutant(@RequestBody Mutante mutante) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body("");
+            return ResponseEntity.status(HttpStatus.OK).body(mutanteService.isMutant(mutante));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("\"error\":\"El ADN ingresado no es de un mutante\"}");
         }
