1e95f159e1a42aa8e526ef9dce4bae4da79370f7
Juan Pablo <jupbiondo@gmail.com>
2024-09-29 12:05:23 -0300

    Create initial isMutant transaction

--- src/main/java/com/jpbiondo/mutanteAPI/services/MutanteService.java
+++ src/main/java/com/jpbiondo/mutanteAPI/services/MutanteService.java
@@ -1,8 +1,13 @@
 package com.jpbiondo.mutanteAPI.services;
 
+import com.jpbiondo.mutanteAPI.entities.Mutante;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MutanteService {
+    @Transactional
+    public boolean isMutant(Mutante mutante){
+        return true;
+    }
 }
