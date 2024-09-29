d3d58b9deea8df683d17a4015e31b06ebd3c17de
Juan Pablo <jupbiondo@gmail.com>
2024-09-29 11:42:07 -0300

    Spring project creation

---
+++ src/main/java/com/jpbiondo/mutanteAPI/MutanteApiApplication.java
@@ -0,0 +1,13 @@
+package com.jpbiondo.mutanteAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutanteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutanteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpbiondo/mutanteAPI/MutanteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpbiondo.mutanteAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutanteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
