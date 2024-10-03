0d3561c28f848cb64371ae89dc264e0e4a3c2306
Juan Pablo <jupbiondo@gmail.com>
2024-10-02 21:09:24 -0300

    Add mutant check algorithm

--- src/main/java/com/jpbiondo/mutanteAPI/services/MutanteService.java
+++ src/main/java/com/jpbiondo/mutanteAPI/services/MutanteService.java
@@ -4,6 +4,8 @@ import com.jpbiondo.mutanteAPI.entities.Mutante;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 import static java.lang.Math.abs;
 
 @Service
@@ -17,17 +19,63 @@ public class MutanteService {
         if(dna.length < 4) return false;
 
 
-        int consecutivesByCol;
-        int[] consecutivesByRow;
-        int[] consecutivesByLRDiagonal;
-        int[] consecutivesByRLDiagonal;
-        return true;
+        int consecutivesByCol = 1;
+        int[] consecutivesByRow = new int[dna.length];
+        int[] consecutivesByLRDiagonal = new int[2*(dna.length - 4) + 1];
+        int[] consecutivesByRLDiagonal = new int[2*(dna.length - 4) + 1];
+
+        Arrays.fill(consecutivesByRow, 1);
+        Arrays.fill(consecutivesByLRDiagonal, 1);
+        Arrays.fill(consecutivesByRLDiagonal, 1);
+
+        int diagElemIndex;
+        int countSequence = 0;
+        final int diagRadius = dna.length-4;
+
+        for(int i = 0; i < dna.length; i++) {
+            for(int j = 0; j < dna[i].length(); j++) {
+
+                //Check for sequences in columns
+                if(j > 0) {
+                    consecutivesByCol = checkConsecutive(dna[i].charAt(j), dna[i].charAt(j-1), consecutivesByCol);
+                    if(isSequence(consecutivesByCol)) countSequence++;
+                }
+
+                if(i > 0){
+                    //Check for sequence in 4 or more chars diagonals from Left to Right
+                    if(j > 0){
+                        if(isAValidDiagonalChar(diagRadius, i, j)) {
+                            diagElemIndex = diagRadius - (i-j);
+                            consecutivesByLRDiagonal[diagElemIndex] = checkConsecutive(dna[i].charAt(j), dna[i-1].charAt(j-1), consecutivesByLRDiagonal[diagElemIndex]);
+                            if(isSequence(consecutivesByLRDiagonal[diagElemIndex])) countSequence++;
+                        }
+                    }
+
+                    //Check for sequence in 4 or more chars diagonals from Right to Left
+                    if(j < dna.length - 1){
+                        if(isAValidDiagonalChar(diagRadius, (dna.length - 1) - i, j)) {
+                            diagElemIndex = diagRadius - ((dna.length - i - 1) - j);
+                            consecutivesByRLDiagonal[diagElemIndex] = checkConsecutive(dna[i].charAt(j), dna[i-1].charAt(j+1), consecutivesByRLDiagonal[diagElemIndex]);
+                            if(isSequence(consecutivesByRLDiagonal[diagElemIndex])) countSequence++;
+                        }
+                    }
+
+                    //Check for sequence in rows
+                    consecutivesByRow[j] = checkConsecutive(dna[i].charAt(j), dna[i-1].charAt(j), consecutivesByRow[j]);
+                    if(isSequence(consecutivesByRow[j])) countSequence++;
+                }
+
+                if(countSequence > 1) return true;
+
+            }
+        }
+        return false;
     }
 
     private boolean isValidDNAFormat(String[] dna){
         int dnaRows = dna.length;
-        for(int i = 0; i < dnaRows; i++) {
-            if(dna[i].length() != dnaRows) return false;
+        for (String s : dna) {
+            if (s.length() != dnaRows) return false;
         }
         return true;
     }
