c7541183c0f29ed33234ca62898413a25438f3ff
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-03 12:04:43 -0300

    feat: add CORS to stats endpoint

--- src/main/java/com/jpbiondo/mutanteAPI/controllers/StatsController.java
+++ src/main/java/com/jpbiondo/mutanteAPI/controllers/StatsController.java
@@ -3,13 +3,14 @@ package com.jpbiondo.mutanteAPI.controllers;
 import com.jpbiondo.mutanteAPI.dtos.StatsDto;
 import com.jpbiondo.mutanteAPI.services.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/stats")
+@CrossOrigin(origins = "*")
 public class StatsController {
 
     @Autowired
